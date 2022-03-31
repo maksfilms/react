76ef813372aa1088fb7bcce84a36fe4e3cccc80c
Maksim <plysheuski.maksim@gmail.com>
2022-03-31 19:24:58 +0400

    use props in components

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -4,18 +4,25 @@ function Rating() {
     console.log("Rating rendering")
     return (
         <div>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={true}/>
+            <Star selected={false}/>
+            <Star selected={false}/>
+
         </div>
     );
 }
 
-function Star() {
+function Star(props: any) {
+    debugger
     console.log('Star rendering')
-    return <span><b>star </b></span>
+    if (props.selected === true) {
+        return <span><b>star </b></span>
+    } else {
+        return <span>star </span>
+    }
+
 }
 
 export default Rating;
