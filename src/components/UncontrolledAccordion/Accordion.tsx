43a920e202847e9f82f403b1264a5491c3c2bf5c
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-05-22 17:21:32 +0300

    working with useState

--- src/components/UncontrolledAccordion/Accordion.tsx
+++ src/components/UncontrolledAccordion/Accordion.tsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 type AccordionPropsType = {
-    titleValue: string,
-    collapsed: boolean
+    titleValue: string
+    //collapsed: boolean
 }
 
-export function Accordion(props: AccordionPropsType) {
-    debugger
-        return <div>
-            <AccordionTitle title={props.titleValue}/>
-            {!props.collapsed && <AccordionBody/>}
-        </div>
+export function UncontrolledAccordion(props: AccordionPropsType) {
+    const [collapsed, setCollapsed] = useState(false)
+
+    return <div>
+        <AccordionTitle title={props.titleValue}/><button onClick={() => {setCollapsed(!collapsed)}}>TOGGLE</button>
+        {!collapsed && <AccordionBody/>}
+    </div>
 }
+
 
 type AccordionTitlePropsType = {
     title: string
