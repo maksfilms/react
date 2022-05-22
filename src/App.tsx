c856bb35e4803bd7dc336aa2db49bb1d909f52cc
Maksim Plysheuski <plysheuski.maksim@gmail.com>
2022-05-22 20:40:11 +0300

    understanding how work with callbacks 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,28 @@
-import React from 'react';
-import './App.css';
-import {Accordion} from './components/Accordion/Accordion'
-import {Rating} from "./components/Rating/Rating";
+import React, {useState} from "react";
+import "./App.css";
+import {Rating, RatingValueType} from "./components/Rating/Rating";
 import OnOff from "./components/OnOff/OnOff";
 import {UncontrolledAccordion} from "./components/UncontrolledAccordion/Accordion";
-import {UncontrolledRating} from "./components/UncontrolledRating/UncontrolledRating";
-
-
-function hello() {
-    debugger
-    alert("Hello IT-KAMASUTRA")
-}
-
-//hello();
+import {Accordion} from "./components/Accordion/Accordion";
 
 function App() {
     console.log("App rendering")
+
+    const [ratingValue, setRatingValue] = useState<RatingValueType>(0)
+    const [accordionCollapsed, setAccordionCollapsed] = useState<boolean>(true)
+
     return (
         <div className={"App"}>
             <OnOff />
-            <OnOff />
-            <OnOff />
-            <OnOff />
 
             <UncontrolledAccordion titleValue={"Menu"}/>
-            <UncontrolledAccordion titleValue={"Menu"}/>
 
 
-            <Rating value={2} />
+            <Rating value={ratingValue} onClick={setRatingValue}/>
 
-            <UncontrolledRating />
+            {/*<UncontrolledRating />*/}
 
-            {/*<Accordion titleValue={"Menu"} collapsed={true} />
-            <Accordion titleValue={"Users"} collapsed={false}/>*/}
+            <Accordion titleValue={"Users"} collapsed={accordionCollapsed} onClick={setAccordionCollapsed}/>
         </div>
     );
 }
--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,30 +1,41 @@
 import React from "react";
 
+
 type AccordionPropsType = {
     titleValue: string,
     collapsed: boolean
+    onClick: (value: boolean) => void
 }
 
 export function Accordion(props: AccordionPropsType) {
     debugger
         return <div>
-            <AccordionTitle title={props.titleValue}/>
+            <AccordionTitle title={props.titleValue} onClick={props.onClick} value={props.collapsed}/>
             {!props.collapsed && <AccordionBody/>}
         </div>
 }
 
+
+
 type AccordionTitlePropsType = {
     title: string
+    onClick: (value: boolean) => void
+    value: boolean
 }
 
 function AccordionTitle(props: AccordionTitlePropsType) {
     debugger
     console.log("AccordionTitle rendering")
-    return <h3>{props.title}</h3>
+    return <h3 onClick={() => {props.onClick(!props.value)}}>{props.title}</h3>
 }
+
+
+
+
 
 function AccordionBody() {
     console.log("AccordionBody rendering")
+
     return <ul>
         <li>1</li>
         <li>2</li>
